d0a62219d1e97009425ce34832e0c1d614291e21
arrides <kos_tet@tut.by>
2013-06-04 11:37:30 +0300

    commit 04.06.0213 11:37

---
+++ sites/all/modules/custom/cassowary_main/js/cassowary_main_placeholder.js
@@ -0,0 +1,6 @@
+(function($) {
+	$(document).ready(function() {
+		$('input#edit-field-link-original-und-0-url').attr("placeholder", "E.g. http://www.direct.com/link/to/content");
+		$('input#edit-field-company-link-node-und-0-url').attr("placeholder", "E.g. http://www.time.com");
+    });
+})(jQuery);
